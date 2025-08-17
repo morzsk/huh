36f0737fc75f5df751157d89bee0d135b25ac482
mo <morzsk@pm.me>
2025-08-17 15:46:49 +0100

    Implemented basic ui layout & command capturing

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,31 @@ use std::time::Duration;
 
 use color_eyre::{Result, eyre::Context};
 use ratatui::{
-    DefaultTerminal, Frame,
+    DefaultTerminal, Frame, TerminalOptions, Viewport,
     crossterm::event::{self, Event, KeyCode},
-    widgets::Paragraph,
+    widgets::{Block, Borders},
 };
+use std::process::Command;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
-    let terminal = ratatui::init();
+    let terminal = ratatui::init_with_options(TerminalOptions {
+        viewport: Viewport::Inline(8),
+    });
     let app = run(terminal).context("app loop failed");
     ratatui::restore();
     app
 }
 
 fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    let command = "cargo updat";
+    let output = Command::new("sh").arg("-c").arg(command).output()?;
+
+    if !output.status.success() {
+        let err = String::from_utf8_lossy(&output.stderr);
+        println!("Huh?: {}", err);
+    }
+
     while !should_quit()? {
         terminal.draw(draw)?;
     }
@@ -23,8 +34,10 @@ fn run(mut terminal: DefaultTerminal) -> Result<()> {
 }
 
 fn draw(frame: &mut Frame) {
-    let greeting = Paragraph::new("Hello World! (press 'q' to quit)");
-    frame.render_widget(greeting, frame.area());
+    let area = frame.area();
+    // let greeting = Paragraph::new("Hello World! (press 'q' to quit)");
+    let block = Block::default().title(" huh? ").borders(Borders::ALL);
+    frame.render_widget(block, area);
 }
 
 fn should_quit() -> Result<bool> {
